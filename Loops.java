322194ecbbbbefdb0c29f54c1121c3ccf8f17c1a
Abhishekshivhare234 <161581455+Abhishekshivhare234@users.noreply.github.com>
2024-11-04 14:27:01 +0530

    changes

--- Loops.java
+++ Loops.java
@@ -1,6 +1,7 @@
 public class Loops {
     public static void main (String [] args){
         for( int i =0;i<5;i++){
+            // here we print the value of i--
             System.out.println(i);
         }
     }
